3fd75d962ff5a3ae9fd3abaf078070e8a50700ee
Tshaan1104 <tshaan1104@gmail.com>
2024-04-06 23:44:09 +0530

    flask repository initiated

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask,make_response
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    resp = make_response({"msg":"hello"})
-    resp.status_code = {204}
-    return resp
---
+++ readme.txt
@@ -0,0 +1,3 @@
+This is the repository for the flask projects 
+cheers 
+
