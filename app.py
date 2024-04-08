2130714203072b53d8d6b66c7f486c8d0c39f874
Tshaan1104 <159934743+Tshaan1104@users.noreply.github.com>
2024-04-08 14:40:52 +0000

    submission of day second

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # print('hello world')
 
 
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return "<p> hello shaan   </p>"
+    return render_template('home.html')
 
 print(__name__)
 if __name__ == '__main__':
