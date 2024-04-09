a4381d67136b9ee3050ec2f4f8674b991f22d5b3
Tshaan1104 <159934743+Tshaan1104@users.noreply.github.com>
2024-04-09 22:51:02 +0000

    Changes made

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ from flask import Flask,render_template,jsonify
 app = Flask(__name__)
 
 job=[
-    {'id':1,'title':'Data Analyst','location':'Bengaluru,India','salary':30000},
-    {'id':2,'title':'Data Analyst','location':'Bengaluru,India','salary':30000},  
+    {'id':1,'title':'Data Analyst','location':'Bengaluru,India','salary':'$30000'},
+    {'id':2,'title':'Data Analyst','location':'Bengaluru,India','salary':'$30000'},  
     {'id':3,'title':'Frontend Engineer','location':'Bengaluru,India'},
-    {'id':1,'title':'Data Analyst','location':'Bengaluru,India','salary':30000},
-    {'id':4,'title':'Data Analyst','location':'Bengaluru,India','salary':30000}]
+    {'id':1,'title':'Data Analyst','location':'Bengaluru,India','salary':'$30000'},
+    {'id':4,'title':'Data Analyst','location':'Bengaluru,India','salary':'$130000'}]
 
 
 @app.route('/')
